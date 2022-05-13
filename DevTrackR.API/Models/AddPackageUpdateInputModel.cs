7a5855f392561dc8ada30f202c77776c069dc70b
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-13 16:04:33 -0300

    Padronização, operador lambda

--- DevTrackR.API/Models/AddPackageUpdateInputModel.cs
+++ DevTrackR.API/Models/AddPackageUpdateInputModel.cs
@@ -3,6 +3,7 @@ namespace DevTrackR.API.Models
     public class AddPackageUpdateInputModel
     {
         public string Status { get; set; }
+
         public bool Delivered { get; set; }
     }
 }
