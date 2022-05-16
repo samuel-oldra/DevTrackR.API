dfcd9ae33db45bb469d76ef27d3eb7e3141012f1
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-16 00:00:00 -0300

    Formatado script de estruturas de controle

--- DevTrackR.Console/Program.cs
+++ DevTrackR.Console/Program.cs
@@ -1,83 +1,112 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
 
-/*
 Console.WriteLine("Digite o seu nome.");
-
 var nome = Console.ReadLine();
 
 Console.WriteLine($"Olá, {nome}");
 
-var umaString = "Essa é uma string";
+// variáveis
+
 var numeroInt = 5;
 var numeroFloat = 5.4f;
 var numeroDouble = 5.4;
 var numeroDecimal = 5.3m;
-var array = new int[3] { 1, 2, 3 };
 var caractere = 'a';
+var umaString = "Essa é uma string";
 var date = new DateTime(1992, 12, 1);
+var array = new int[3] { 1, 2, 3 };
+
+// if-else
+
+Console.WriteLine("Utilizando if-else");
 
 Console.WriteLine("Digite uma opção de 1 a 3.");
 var opcao = Console.ReadLine();
 
-if (opcao == "1") {
+if (opcao == "1")
+{
     Console.WriteLine("Você entrou no menu de Cadastro.");
-} else if (opcao == "2") {
+}
+else if (opcao == "2")
+{
     Console.WriteLine("Você entrou no menu de Reclamação.");
-} else if (opcao == "3") {
+}
+else if (opcao == "3")
+{
     Console.WriteLine("Você entrou no menu de Atendimento de Suporte.");
-} else {
+}
+else
+{
     Console.WriteLine("Opção inválida.");
 }
 
-switch(opcao) {
+// switch-case
+
+Console.WriteLine("Utilizando switch-case");
+
+switch (opcao)
+{
     case "1":
         Console.WriteLine("Você entrou no menu de Cadastro.");
         break;
-
     case "2":
         Console.WriteLine("Você entrou no menu de Reclamação.");
         break;
-
     case "3":
         Console.WriteLine("Você entrou no menu de Atendimento de Suporte.");
         break;
-
     default:
         Console.WriteLine("Opção inválida.");
         break;
 }
 
+// for
+
+Console.WriteLine("Utilizando for");
+
 var valores = Console.ReadLine(); // 1 2 3 4 5
 var valoresArray = valores.Split(" "); // [ "1", "2", "3", "4", "5" ]
 
-for (var i = 0; i < valoresArray.Length; i++) {
+for (var i = 0; i < valoresArray.Length; i++)
+{
     Console.Write(valoresArray[i] + " ");
 }
 
-Console.WriteLine();
+// while
+
+Console.WriteLine("Utilizando while");
 
 var contador = 0;
 
-while(contador < valoresArray.Length) {
+while (contador < valoresArray.Length)
+{
     Console.Write(valoresArray[contador] + " ");
 
     contador++;
 }
 
-Console.WriteLine();
+// foreach
 
-foreach (var item in valoresArray) {
+Console.WriteLine("Utilizando foreach");
+
+foreach (var item in valoresArray)
+{
     Console.Write(item + " ");
 }
 
+// list
+
+Console.WriteLine("Utilizando list");
+
 var notas = new List<int> { 10, 5, 3, 2, 10, 4, 5, 6, 8, 2 };
 
-var anyNota1 = notas.Any(n => n == 1);
-var firstNota10 = notas.First(n => n == 10);
 var singleNota8 = notas.Single(n => n == 8);
+var firstNota10 = notas.First(n => n == 10);
+var anyNota1 = notas.Any(n => n == 1);
 var orderedNotas = notas.OrderBy(n => n);
-var max = notas.Max();
+
 var min = notas.Min();
+var max = notas.Max();
 var sum = notas.Sum();
 var average = notas.Average();
 
@@ -86,10 +115,14 @@ Console.WriteLine($"Min: {min}");
 Console.WriteLine($"Sum: {sum}");
 Console.WriteLine($"Average: {average}");
 
-foreach (var nota in orderedNotas) {
+foreach (var nota in orderedNotas)
+{
     Console.Write(nota + " ");
 }
+
 */
+
+// TODO: Formatar e organizar o programa console.
 
 var pacotes = new List<Pacote>();
 
